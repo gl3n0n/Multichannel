54c445ab8d531b6f246a4bda5a4509ad6a31cff8
Mark Raymund Neis Tejero <bayugyug@gmail.com>
2015-08-17 21:49:58 +0800

    #Scheduled Post - fix of index failure for submit/update
    #Scheduled Post - fix of index failure for submit/update

--- protected/controllers/ScheduledPostController.php
+++ protected/controllers/ScheduledPostController.php
@@ -170,6 +170,7 @@ class ScheduledPostController extends Controller
 			$model->attributes=$_POST['ScheduledPost'];
 			
 			$chans = @preg_split("/[\-]/", trim($_POST['ScheduledPost']['ChannelId']));
+			if(@count($chans)>=2)
 			$model->setAttribute("ChannelId", $chans[1]);
 			$model->setAttribute("DateCreated", new CDbExpression('NOW()'));
 			$model->setAttribute("UpdatedBy", Yii::app()->user->id);
