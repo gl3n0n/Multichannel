4db9d92a30fe51a7562844637ed68b5882d75780
bayugyug <bayugyug@gmail.com>
2015-09-04 10:23:32 +0800

    #Misc - CHECKBOX  [ check-all or uncheck-all ] - add trigger change
    #Misc - CHECKBOX  [ check-all or uncheck-all ] - add trigger change
    
    Make sure CHECK ALL function is working for creating new entries

--- protected/components/view/checkboxlist.php
+++ protected/components/view/checkboxlist.php
@@ -44,6 +44,7 @@ $idCheckNone  = "{$elemIdPrefix}_chkNone";
     .on("click", function(e) {
       e.preventDefault();
       $(<?php echo $elemIdPrefix; ?>).find("[id^=<?php echo $elemIdPrefix; ?>]").prop("checked", true);
+      $(<?php echo $elemIdPrefix; ?>).find("[id^=<?php echo $elemIdPrefix; ?>]").prop("checked", true).trigger("change");
     });
 
   $("#<?=$idCheckNone?>")
@@ -51,5 +52,6 @@ $idCheckNone  = "{$elemIdPrefix}_chkNone";
     .on("click", function(e) {
       e.preventDefault();
       $(<?php echo $elemIdPrefix; ?>).find("[id^=<?php echo $elemIdPrefix; ?>]").prop("checked", false);
+      $(<?php echo $elemIdPrefix; ?>).find("[id^=<?php echo $elemIdPrefix; ?>]").prop("checked", false).trigger("change");
     });
 </script>
