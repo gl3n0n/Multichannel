1061b3d675f220263c5d3632aa57022967fb60d7
Mark Raymund Neis Tejero <bayugyug@gmail.com>
2015-09-08 23:22:07 +0800

    #Audit Logs remove from Login Module
    #Audit Logs remove from Login Module

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -103,8 +103,8 @@ class SiteController extends Controller
 			// validate user input and redirect to the previous page if valid
 			if($model->validate() && $model->login())
 				//$this->redirect(Yii::app()->user->returnUrl);
-				$utilLog = new Utils;
-				$utilLog->saveAuditLogs();
+				//$utilLog = new Utils;
+				//$utilLog->saveAuditLogs();
 
 				if (Yii::app()->user->AccessType == 'SUPERADMIN')
 				{
